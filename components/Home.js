6b8ed6bfc0bdd7ceed5e21e32457f80265617d84
Joan Mas <joan.mas23@gmail.com>
2023-08-24 16:40:56 +0200

    first commit

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,31 @@
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
+import BgVideo from "../assets/background.mp4";
 
 function Home() {
   return (
     <div>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+        rel="stylesheet"
+      />
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <video autoPlay loop muted className={styles.video}>
+          <source src={BgVideo} type="video/mp4" />
+        </video>
+
+        <div className={styles.container}>
+          <div className={styles.header}>
+            <div className={styles.iconNav}>
+              <span class="material-symbols-outlined">menu</span>
+            </div>
+
+            <div className={styles.idTitle}>
+              <h1 className={styles.title}>Joan Mas</h1>
+              <h2 className={styles.underTitle}>Developer</h2>
+            </div>
+
+          </div>
+        </div>
       </main>
     </div>
   );
--- next.config.js
+++ next.config.js
@@ -1,9 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [],
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: [],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+// next.config.js
+module.exports = {
+  // ...
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/i, // Ajoutez tous les formats vidéo que vous utilisez
+      use: [
+        {
+          loader: 'url-loader', // Utilisez 'file-loader' si vous l'avez choisi
+          options: {
+            limit: 8192, // Limite pour l'intégration des petits fichiers directement dans le bundle
+            publicPath: '/_next/static/videos', // Chemin public pour les vidéos
+            outputPath: 'static/videos', // Chemin dans le build
+            name: '[name].[hash].[ext]',
+          },
+        },
+      ],
+    });
+
+    return config;
   },
 };
-
-module.exports = nextConfig;
