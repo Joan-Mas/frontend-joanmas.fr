baad36b9d921d1fcb8bc83b2c2fba64c9e86c1a1
Joan Mas <joan.mas23@gmail.com>
2023-09-21 10:57:15 +0200

    responsive+canonicalurl

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
@@ -11,11 +12,16 @@ const store = configureStore({
 });
 
 function App({ Component, pageProps }) {
+
+  const router = useRouter();
+  const canonicalUrl = (`https://joanmas.fr` + (router.asPath === "/" ? "": router.asPath)).split("?")[0];
+
   return (
  
       <Provider store={store}>
         <Head>
           <title>Joan Mas</title>
+          <link rel='canonical' href={canonicalUrl}/>
           <meta name="description" content="I am a javaScript fullstack developer junior, this is my portfolio."/>
         </Head>
         <Component {...pageProps} />
